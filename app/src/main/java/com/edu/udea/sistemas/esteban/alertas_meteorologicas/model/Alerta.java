45d191bc80c1bb81c28b5083c333d7dbbfa9d646
exteban34 <exteban34@gmail.com>
2015-05-18 12:22:00 -0500

    cambiar el modelo de alarma, mejorar la vista para generar alarma

--- app/src/main/java/com/edu/udea/sistemas/esteban/alertas_meteorologicas/model/Alerta.java
+++ app/src/main/java/com/edu/udea/sistemas/esteban/alertas_meteorologicas/model/Alerta.java
@@ -6,44 +6,74 @@ import java.util.Date;
  * Created by esteban on 28/04/2015.
  */
 public class Alerta {
-    private int temperatura;
-    private int humedad;
-    private int velocidad;
+    private int temperaturaBajo;
+    private int temperaturaAlto;
+    private int humedadBajo;
+    private int humedadAlto;
+    private int luzBajo;
+    private int luzAlto;
     private Date fecha;
 
-    public Alerta(int temperatura, int humedad, int velocidad, Date fecha) {
-        this.temperatura = temperatura;
-        this.humedad = humedad;
-        this.velocidad = velocidad;
+    public Alerta(int temperaturaBajo, int temperaturaAlto, int humedadBajo, int humedadAlto, int luzBajo, int luzAlto, Date fecha) {
+        this.temperaturaBajo = temperaturaBajo;
+        this.temperaturaAlto = temperaturaAlto;
+        this.humedadBajo = humedadBajo;
+        this.humedadAlto = humedadAlto;
+        this.luzBajo = luzBajo;
+        this.luzAlto = luzAlto;
         this.fecha = fecha;
-    }
-
-    public int getTemperatura() {
-        return temperatura;
-    }
-
-    public void setTemperatura(int temperatura) {
-        this.temperatura = temperatura;
-    }
-
-    public int getHumedad() {
-        return humedad;
-    }
-
-    public void setHumedad(int humedad) {
-        this.humedad = humedad;
-    }
-
-    public int getVelocidad() {
-        return velocidad;
-    }
-
-    public void setVelocidad(int velocidad) {
-        this.velocidad = velocidad;
     }
 
     public Date getFecha() {
         return fecha;
+    }
+
+    public int getTemperaturaBajo() {
+        return temperaturaBajo;
+    }
+
+    public int getTemperaturaAlto() {
+        return temperaturaAlto;
+    }
+
+    public int getHumedadBajo() {
+        return humedadBajo;
+    }
+
+    public int getHumedadAlto() {
+        return humedadAlto;
+    }
+
+    public int getLuzBajo() {
+        return luzBajo;
+    }
+
+    public int getLuzAlto() {
+        return luzAlto;
+    }
+
+    public void setTemperaturaBajo(int temperaturaBajo) {
+        this.temperaturaBajo = temperaturaBajo;
+    }
+
+    public void setTemperaturaAlto(int temperaturaAlto) {
+        this.temperaturaAlto = temperaturaAlto;
+    }
+
+    public void setHumedadBajo(int humedadBajo) {
+        this.humedadBajo = humedadBajo;
+    }
+
+    public void setHumedadAlto(int humedadAlto) {
+        this.humedadAlto = humedadAlto;
+    }
+
+    public void setLuzBajo(int luzBajo) {
+        this.luzBajo = luzBajo;
+    }
+
+    public void setLuzAlto(int luzAlto) {
+        this.luzAlto = luzAlto;
     }
 
     public void setFecha(Date fecha) {
